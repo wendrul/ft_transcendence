e61e683f2027511166c533ddcff15abbf6be41b6
wendrul <dethomassin.etienne@gmail.com>
2022-06-17 17:48:22 +0200

    Add squashing on bounce state

--- srcs/react-frontend/src/game/game_objects/Ball.ts
+++ srcs/react-frontend/src/game/game_objects/Ball.ts
@@ -4,21 +4,30 @@ import { Graphics } from "pixi.js";
 import addKeyListeners from "../util/Interaction";
 import { ICollider, Ray } from "../util/Collider";
 
+enum States {
+  MOVING,
+  BOUNCE_STALL,
+}
+
 class Ball {
   pos: Vector2;
-
+  static readonly States = States;
   velocity: Vector2;
+
+  state: States = States.MOVING;
+  enteringState: boolean = false;
+  elapsedTime: number = 0;
 
   private _gfx: Graphics;
   private _app: PIXI.Application;
   colliders: Array<ICollider>;
 
   static readonly radius = 15;
+  static readonly bounceStallDelay = 0.05;
 
   constructor(app: PIXI.Application) {
     this.pos = new Vector2(app.renderer.width / 2, app.renderer.height / 2);
     this.velocity = new Vector2(10, -20);
-
     this._gfx = new Graphics();
     this._app = app;
 
@@ -30,48 +39,84 @@ class Ball {
     this._app.stage.addChild(this._gfx);
     const k_Listener = addKeyListeners("k");
     k_Listener.press = () => {
+      // this.velocity = this.velocity.normalized().scale(1000);
       this.setRandomVelocity();
     };
+  }
+
+  private update(delta: number) {
+    this.redraw();
+
+    switch (this.state) {
+      case States.BOUNCE_STALL:
+        if (this.enteringState) {
+          this.enteringState = false;
+          this.elapsedTime = 0;
+        }
+        const t = this.elapsedTime / Ball.bounceStallDelay;
+        // this._gfx.x = this.pos.x;
+        // this._gfx.y = this.pos.y;
+        this._gfx.scale.set(1,0.5);
+        this.elapsedTime += delta / 60;
+        if (this.elapsedTime > Ball.bounceStallDelay) {
+          this.state = States.MOVING;
+          this.enteringState = true;
+        }
+        break;
+
+      case States.MOVING:
+        this.enteringState = false;
+        this._gfx.scale.set(1,1);
+        // this._gfx.x = 0;
+        // this._gfx.y = 0;
+
+        const newPos = this.pos.add(this.velocity.scale(delta / 60));
+
+        // gravity:
+        this.velocity = this.velocity.subtract(
+          new Vector2(0, (-1500 * delta) / 60)
+        );
+
+        const collisionPoint = this.findPossibleCollision(
+          this.pos,
+          newPos,
+          this.colliders
+        );
+
+        //Avoid going over the wall by going exactly to the point where it would collide
+        if (collisionPoint != null) {
+          this.pos = collisionPoint;
+        } else {
+          this.pos = newPos;
+        }
+        break;
+      default:
+        break;
+    }
   }
 
   redraw() {
     this._gfx.clear();
 
     this._gfx
+      .moveTo(this.pos.x, this.pos.y)
       .beginFill(0xfffffff)
-      .drawCircle(this.pos.x, this.pos.y, Ball.radius)
+      .drawCircle(Ball.radius, Ball.radius, Ball.radius)
       .endFill();
+    this._gfx.x = this.pos.x;
+    this._gfx.y = this.pos.y;
+    this._gfx.pivot.x = Ball.radius;
+    this._gfx.pivot.y = Ball.radius;
+
 
     if (globalThis.debugMode) {
       const col_start = this.pos;
-      const col_end = this.velocity.scale(0.3).add(col_start);
+      const col_end = this.velocity.scale(0.1).add(new Vector2(Ball.radius, Ball.radius));
       this._gfx
-        .moveTo(col_start.x, col_start.y)
+        .moveTo(Ball.radius,Ball.radius)
         .lineStyle(3, 0xfcdb03)
         .lineTo(col_end.x, col_end.y)
         .endFill();
-    }
-  }
-
-  private update(delta: number) {
-    this.redraw();
-
-    const newPos = this.pos.add(this.velocity.scale(delta / 60));
-    this.velocity = this.velocity.subtract(
-      new Vector2(0, (-1500 * delta) / 60)
-    );
-
-    const collisionPoint = this.findPossibleCollision(
-      this.pos,
-      newPos,
-      this.colliders
-    );
-
-    //Avoid going over the wall by going exactly to the point where it would collide
-    if (collisionPoint != null) {
-      this.pos = collisionPoint;
-    } else {
-      this.pos = newPos;
     }
   }
 
@@ -102,6 +147,8 @@ class Ball {
 
     if (chosenCollider != null) {
       chosenCollider.collider.onCollision(this);
+      this.state = States.BOUNCE_STALL;
+      this.enteringState = true;
       return chosenCollider.inter;
     }
     return null;
