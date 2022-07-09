130c763a9e998e1cd7c96bb0a67f658c0c49ea01
42corozco <42corozco@gmail.com>
2022-07-09 20:39:48 +0200

    update signin page

--- srcs/react-frontend/src/pages/SignIn/SignIn.tsx
+++ srcs/react-frontend/src/pages/SignIn/SignIn.tsx
@@ -1,8 +1,14 @@
 import react, { useEffect } from 'react';
+
 import {
-	MDBBtn
+	MDBBtn,
+	MDBCheckbox,
+	MDBRow,
+	MDBCol,
+	MDBInput
   } from 'mdb-react-ui-kit';
 import "./SignIn.css";
+
 
 function SignIn() {
 	useEffect(() => {
@@ -17,9 +23,28 @@ function SignIn() {
 					If you don't have an account
 				</p>
 				<div className="d-flex flex-column align-items-center">
-				<MDBBtn color='primary' href="#">Register here</MDBBtn>
+				<MDBBtn color='primary' href="/signup">Register here</MDBBtn>
 				</div>
 			</div>
+
+	{/* intentando con https://mdbootstrap.com/docs/b5/react/forms/overview/ */}
+			<div className="d-flex flex-column align-items-center justify-content-center w-75 pb-5 mb-3">
+			<form>
+				<MDBInput className='mb-4' type='email' id='form1Example1' label='Email address' />
+				<MDBInput className='mb-4' type='password' id='form1Example2' label='Password' />
+
+				<MDBRow className='mb-4'>
+				<MDBCol>
+					<a href='#!'>Forgot password?</a>
+				</MDBCol>
+				</MDBRow>
+
+				<MDBBtn type='submit' block>
+				Sign in
+				</MDBBtn>
+			</form>
+			</div>
+
 
 		</div>
 	</div>
