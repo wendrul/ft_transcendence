4ec8f01f2d212cd8923ebc60ad77b501573a3e52
42corozco <42corozco@gmail.com>
2022-07-14 18:35:45 +0200

    conectando el axion

--- srcs/nestjs-backend/src/main.ts
+++ srcs/nestjs-backend/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  app.enableCors();
 
   await app.listen(3000);
 }
--- srcs/react-frontend/src/pages/SignIn/SignIn.tsx
+++ srcs/react-frontend/src/pages/SignIn/SignIn.tsx
@@ -1,4 +1,7 @@
-import react, { useEffect } from 'react';
+import React, { useEffect } from 'react';
+
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 import {
 	MDBBtn,
@@ -9,19 +12,35 @@ import {
 	MDBInput
   } from 'mdb-react-ui-kit';
 import "./SignIn.css";
-
+/*
+interface IPost {
+    firstName: string;
+    LastName: string;
+  }
+const defaultPosts:IPost[] = [];
+*/
 
 function SignIn() {
+
+//	const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(defaultPosts);
 	useEffect(() => {
-		document.title = "SigIn";  
+		document.title = "SigIn";
 	}, []);
+
+	axios.get(`http://localhost:3002/users`)
+	.then(res => {
+	  const persons = res.data;
+		console.log(res.data);
+	})
+
+
   return (
 	<div className="p-5 row bd-highlight justify-content-center">
 		<div className="p-2 d-flex flex-column bd-highlight col-example col-md-6 align-items-center justify-content-center h-100">
 
 			<div className="d-flex flex-column align-items-center justify-content-center w-75 pb-5 mb-3">
 				<p className="register_btn mb-3">
-					If you don't have an account
+					If you don't have an account 
 				</p>
 				<div className="d-flex flex-column align-items-center">
 					<MDBBtn color='primary' href="/signup">Register here</MDBBtn>
