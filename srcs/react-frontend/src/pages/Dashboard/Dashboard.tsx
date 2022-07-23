7aafadb1e81c4073f5abc18ebbe9e898c96981c9
Eric LING <eric_ling@hotmail.fr>
2022-07-23 18:42:10 +0200

    dashboard done

--- srcs/react-frontend/src/pages/Dashboard/Dashboard.tsx
+++ srcs/react-frontend/src/pages/Dashboard/Dashboard.tsx
@@ -22,7 +22,19 @@ function guestView(){
 
 function userView(){
 
-	return (<h1>user view</h1>);
+	return (
+		<>
+		<div className="d-flex flex-row mt-4">
+			<div className="dboard-avatar shadow-lg rounded d-flex flex-column align-items-center m-4">
+				<img className='user' src={img_user} alt='user'></img>
+				<h3 className='text-dark mt-4'> Ranking #1</h3>
+				<h5 className='text-dark'> Pseudo</h5>
+			</div>
+		</div>
+		<button className='m-3 dboard-btn-sin bg-warning display-6'>PLAY !</button>
+		<button className='m-3 dboard-btn-sup'>CREATE ROOM</button>
+	</>
+	);
 }
 
 function Dashboard(){
@@ -37,52 +49,106 @@ function Dashboard(){
 
 			<div className='bc-blue d-flex flex-row align-items-center justify-content-center border-start border-2 border-dark w-75'>
 
-				<div className='dboard-div-scroll border  bg-white border rounded mb-5'>
-					<div className='d-flex flex-column mt-3 ms-4 m-4 mb-4'>
-
-						<div className='d-flex flex-row align-items-center justify-content-between'>
-							<div className="input-group w-50 mb-3">
-						  	<span className="input-group-text bc-gr text-white" id="basic-addon1">Search</span>
-	  						<input type="text" className="form-control " placeholder="pseudo" aria-label="pseudo" aria-describedby="basic-addon1"/>
+				<div className='d-flex flex-row'>
+					<div className='dboard-div-scroll border bg-white border rounded mb-5'>
+						<form className='filter'>
+							<div className='d-flex justify-content-center mt-5'>
+								<input className='filter-search' type="text" placeholder="Search.."/>
 							</div>
-							
-							<button className='bg-white border border-white' data-bs-toggle="tooltip" data-bs-placement="right" title="Filter">
-								<img className="img_gear"src={img_gear} alt='gear'></img>
-							</button>
-						</div>
+							<div className='mt-2'>
+								<h4 className='ms-3 mt-2 mb-0'>Status</h4>
+								<input className='ms-3 me-1' type="radio" name='status'/>
+								<label>connected</label>
+							</div>
+							<div>
+								<input className='ms-3 me-1' type="radio" name='status'/>
+								<label>playing</label>
+							</div>
+							<div>
+								<input className='ms-3 me-1' type="radio" name='status'/>
+								<label>offline</label>
+							</div>								
+							<div>
+								<h4 className='ms-3 mt-2 mb-0'>Rank</h4>
+								<input className='ms-3 me-1' type="radio" name='rank'/>
+								<label>highest rank</label>
+							</div>
+							<div>
+								<input className='ms-3 me-1' type="radio" name='rank'/>
+								<label>lowest rank</label>
+							</div>
+							<div>
+							<h4 className='ms-3 mt-2 mb-0'>Score</h4>
+							<input className='ms-3 me-1' type="radio" name='score'/>
+								<label>highest score</label>
+							</div>
+							<div>
+								<input className='ms-3 me-1' type="radio" name='score'/>
+								<label>lowest score</label>
+							</div>
+						</form>
+						<div className='dboard-tab'>
 
-
-						<div className='div-score bc-red'>
-							<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
-						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
-						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
-						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
-						<div className='div-score bc-red'>
-							<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
-						<div className='div-score bc-red'>
-							<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
-						<div className='div-score bc-green'>
-							<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
-						</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
 					</div>
-				</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>										
+					<div className='div-score bc-red'>
+						<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-red'>
+						<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-red'>
+						<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>						
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-green'>
+						<h3> WIN 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>
+					<div className='div-score bc-red'>
+						<h3> LOSE 10 - 3 PSEUDO #2 25/12/2002 14:43 </h3>
+					</div>						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						</div>
+
+
+					</div>
+		 		</div>
+
+
+
 			</div>
 		</div>
 	);
