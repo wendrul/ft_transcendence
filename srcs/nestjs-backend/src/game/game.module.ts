3fd3587a55d39dcb6a6d9cf6f966a7483392b557
42corozco <42corozco@gmail.com>
2022-09-09 11:58:27 +0200

    solved dependencies GameModule

--- srcs/nestjs-backend/src/game/game.module.ts
+++ srcs/nestjs-backend/src/game/game.module.ts
@@ -4,14 +4,32 @@ import {CurrentUserMiddleware} from "src/users/middleware/current-user.middlewar
 import {Match} from "./entities/match.entity";
 import {GameService} from "./game.service";
 import {User} from "../users/entities/users.entity";
+import { UsersService } from "src/users/users.service";
+import { LocalFilesService } from "src/users/localFiles.service";
+import { Channel } from "src/chat/entities/channels.entity";
+import { Message } from "src/chat/entities/messages.entity";
+import { LocalFile } from "src/users/entities/localFiles.entity";
+import { AdminsInChannels } from "src/chat/entities/adminsInChannels.entity";
+import { UsersInChannels } from "src/chat/entities/usersInChannels.entity";
+import { BlockedUser } from "src/users/entities/blockedUsers.entity";
 
 @Module({
 	imports: [
 		TypeOrmModule.forFeature([Match]),
 		TypeOrmModule.forFeature([User]),
+
+		TypeOrmModule.forFeature([Channel]),
+		TypeOrmModule.forFeature([Message]),
+		TypeOrmModule.forFeature([LocalFile]),
+		TypeOrmModule.forFeature([AdminsInChannels]),
+		TypeOrmModule.forFeature([UsersInChannels]),
+		TypeOrmModule.forFeature([BlockedUser]),
+
 	],
 	providers: [
 		GameService,
+		UsersService,
+		LocalFilesService,
 	]
 })
 
